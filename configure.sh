113f6ca91a2237e5309135f5c4bd2e0c73274208
Reuben Cherian <reuben.cherian@icloud.com>
2024-06-25 14:17:28 -0400

    changes to .dotfiles

--- configure.sh
+++ configure.sh
@@ -4,5 +4,9 @@
 # terminal (iterm2) colors, fonts using dynamic profiles
 # vscode config file json - has builtin sync (turn it on)
 # dotfiles
+git clone https://github.com/Reucherian/.dotfiles.git $HOME/.dotfiles
+cd $HOME/.dotfiles
+stow zsh
+stow starship
 # folder icons for specific folders
-# 
+#
