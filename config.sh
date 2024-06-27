a5ad6f41ca09907e2d8793f3d0017a28bc81af88
Reuben Cherian <reuben.cherian@icloud.com>
2024-06-27 10:12:40 -0400

    rename iterm2 profile and editing config

--- config.sh
+++ config.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
-# things to configure -
-# terminal (iterm2) colors, fonts using dynamic profiles
-# vscode config file json - has builtin sync (turn it on)
-# dotfiles
-if [ -d $HOME/.dotfiles ]; then cd $HOME/.dotfiles && ./config.sh; else git clone https://github.com/Reucherian/.dotfiles.git $HOME/.dotfiles; cd $HOME/.dotfiles && ./config.sh; fi
+# terminal (iterm2) colors, fonts u
+# using dynamic profiles
+echo "sign into 👨🏻‍💻 visual studio code with setting sync using the github account for Reucherian"
+if [ -d $HOME/.dotfiles ]; then cd $HOME/.dotfiles && ./config.sh; else git clone https://github.com/Reucherian/.dotfiles.git $HOME/.dotfiles; cd $HOME/.dotfiles && ./config.sh; fi # dotfiles
 # folder icons for specific folders
-#
