8114e874988c04aa8a4acf7f6c8aa56b8c375a85
Reuben Cherian <reuben.cherian@icloud.com>
2024-06-27 10:06:48 -0400

    fixing the script to correctly configure dotfiles

--- config.sh
+++ config.sh
@@ -4,6 +4,6 @@
 # terminal (iterm2) colors, fonts using dynamic profiles
 # vscode config file json - has builtin sync (turn it on)
 # dotfiles
-if [ -d $HOME/.dotfiles ] then; cd $HOME/.dotfiles && ./config.sh; else git clone https://github.com/Reucherian/.dotfiles.git $HOME/.dotfiles && cd $HOME/.dotfiles && ./config.sh; fi;
+if [ -d $HOME/.dotfiles ]; then cd $HOME/.dotfiles && ./config.sh; else git clone https://github.com/Reucherian/.dotfiles.git $HOME/.dotfiles; cd $HOME/.dotfiles && ./config.sh; fi
 # folder icons for specific folders
 #
