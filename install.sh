e69784856c546b4c39786ba3e71ca58079d99809
Reuben Cherian <reuben.cherian@icloud.com>
2024-07-03 23:23:53 -0400

    fix to install packages

--- install.sh
+++ install.sh
@@ -6,42 +6,60 @@ check_install() {
         if [ -x "$(command -v $1)" ]; then
             echo "The command $1 is already installed."
         else
-            $2
+            $pkg_mngr $pkg_mngr_cmd $2
         fi
         ;;
     "app")
         if [ -d "/Applications/$1.app" ]; then
             echo "The application $1 is already installed."
         else
-            $2
+	    $pkg_mngr $pkg_mngr_cmd $pkg_mngr_cmd_cask $2
         fi
         ;;
-    "brew")
-        if [ -L "/opt/homebrew/opt/$1" ]; then
+    "mngr")
+        if [ -L "${pkg_mngr_path}${1}" ]; then
             echo "The package $1 is already installed and linked."
         else
-            $2
+	   $pkg_mngr $pkg_mngr_cmd $2 
         fi
         ;;
     esac
 }
 
+pkg_mngr_convert(){
+    local -n result=$1
+    if [ $pkg_mngr = "brew" ]; then
+	result=(install $2)
+    else
+	echo "This package manager is not supported"
+	exit
+    fi
+}
+
+brew_init(){
+    pkg_mngr=brew
+    pkg_mngr_path="/opt/homebrew/opt/"
+    pkg_mngr_cmd=install
+    pkg_mngr_cmd_cask=--cask
+}
+
 main() {
+    brew_init
     # does not require command line tools installation since git will need command line tools
-    check_install "tmux" "brew install tmux" "cli" # terminal multiplexer
-    check_install "tpm" "brew install tpm" "brew" # terminal multiplexer package manager
-    check_install "fzf" "brew install fzf" "cli" # fuzzy finder command line utility
-    check_install "nvim" "brew install neovim" "cli" # terminal code editor
+    check_install "tmux" tmux "cli" # terminal multiplexer
+    check_install "tpm" tpm "mngr" # terminal multiplexer package manager
+    check_install "fzf"  fzf "cli" # fuzzy finder command line utility
+    check_install "nvim" neovim "cli" # terminal code editor
     # installing applications
-    check_install "Arc" "brew install --cask arc" "app"  # browser
-    check_install "Visual Studio Code" "brew install --cask visual-studio-code" "app" # graphical code editor
-    check_install "1Password" "brew install --cask 1password" "app" # password manager
-    check_install "Swish" "brew install --cask swish" "app" # window manager
-    check_install "Bartender 5" "brew install --cask bartender" "app" # menu bar manager
-    check_install "IINA" "brew install --cask iina" "app" # media player
-    check_install "iTerm" "brew install --cask iterm2" "app" # terminal app
-    check_install "logioptionsplus" "brew install --cask logi-options-plus" "app" # peripherals app
-    # check_install "" "" "font"
+    check_install "Arc" arc "app"  # browser
+    check_install "Visual Studio Code" visual-studio-code "app" # graphical code editor
+    check_install "1Password" 1password "app" # password manager
+    check_install "Swish" swish "app" # window manager
+    check_install "Bartender 5" bartender "app" # menu bar manager
+    check_install "IINA" iina "app" # media player
+    check_install "iTerm" iterm2 "app" # terminal app
+    check_install "logioptionsplus" logi-options-plus "app" # peripherals app
+    # check_install "" "" "font
 }
 
 main
