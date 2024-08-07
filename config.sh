e067bc2ababf5d85f34301f690a15172ccc15239
Reuben Cherian <reuben.cherian@icloud.com>
2024-08-07 13:15:32 -0400

    colorful and better prompting during setup

--- config.sh
+++ config.sh
@@ -1,32 +1,45 @@
 #!/bin/zsh
-prompt_next(){
-	local progress_next="n"
-	while [ "$progress_next" != "y" ]
-	do
-		echo -n "continue? "
-		read -k 1 progress_next
+autoload -U colors && colors
+if read -qs "?configure $fg_bold[yellow]󰟵 1password$reset_color (y/n)"; then
+	echo ""	
+	echo "opening application ..."
+	open "/Applications/1Password.app"
+	echo "=> enable ssh agent "
+	echo "=> enable command line agent"
+	read -s -k 1 "?press any key to continue ..."
+	echo ""
+else	
+	echo ""
+fi
+
+if read -qs "?configure $fg_bold[green] dotfiles$reset_color (y/n) "; then
+	echo ""
+	echo "setting up ..."
+	if [ -d $HOME/.dotfiles ]; then cd $HOME/.dotfiles && ./config.sh; else git clone git@github.com:Reucherian/.dotfiles.git $HOME/.dotfiles; cd $HOME/.dotfiles && ./config.sh; fi # dotfiles
+	read -s -k 1 "?press any key to continue ..."
+	echo ""
+
+	if read -qs "?configure $fg_bold[cyan]󱋖 icloud sync$reset_color (y/n) "; then
 		echo ""
-	done
-}
+		isync
+	else
+		echo ""
+	fi
+else
+	echo ""
 
-# terminal (iterm2) colors, fonts u
-# using dynamic profiles
+fi
 
-echo "opening 󰟵 1password"
-echo "enable ssh agent and command line agent"
-sleep 5
-open "/Applications/1Password.app"
-prompt_next
 
-echo "configuring  dotfiles"
-if [ -d $HOME/.dotfiles ]; then cd $HOME/.dotfiles && ./config.sh; else git clone git@github.com:Reucherian/.dotfiles.git $HOME/.dotfiles; cd $HOME/.dotfiles && ./config.sh; fi # dotfiles
-echo "done"
-
-echo "opening   visual studio code" 
-echo "enable setting sync with github"
-sleep 5
-open "/Applications/Visual Studio Code.app"
-prompt_next
+if read -qs "?configure $fg_bold[blue]  visual studio code$reset_color (y/n) "; then
+	echo ""
+	open "/Applications/Visual Studio Code.app"
+	echo "=> enable setting sync with github"
+	read -s -k 1 "?press any key to continue ..."
+	echo ""
+else
+	echo ""
+	fi
 
 
 # folder icons for specific folders
